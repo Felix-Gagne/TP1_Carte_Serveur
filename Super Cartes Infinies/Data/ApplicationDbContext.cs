57349f41a8457a8db62203c6e89e221e78b7bec3
Hugo Otth <1968678@cegepmontpetit.ca>
2023-09-01 13:56:42 -0400

    CONFIG: Ajout de 10 cartes dans le seed

--- Super Cartes Infinies/Data/ApplicationDbContext.cs
+++ Super Cartes Infinies/Data/ApplicationDbContext.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
 using Super_Cartes_Infinies.Models;
+using System.Numerics;
 using System.Reflection.Emit;
 using static System.Net.WebRequestMethods;
 
@@ -45,8 +47,6 @@ public class ApplicationDbContext : IdentityDbContext
             new IdentityUserRole<string> { UserId = admin.Id, RoleId = "1" }
         );
 
-
-
         builder.Entity<Match>()
             .HasOne(m => m.PlayerDataA)
             .WithMany()
@@ -56,8 +56,103 @@ public class ApplicationDbContext : IdentityDbContext
             .HasOne(m => m.PlayerDataB)
             .WithMany()
             .OnDelete(DeleteBehavior.NoAction);
-    }
 
+        //Cartes de jeu
+        const String imageUrlBase = "https://upload.wikimedia.org/wikipedia/commons/3/35/Basic_human_drawing.png";
+
+        Card C1 = new Card
+        {
+            Id = 1,
+            Name = "Stickly Steve",
+            Attack = 3,
+            Defense = 3,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C1);
+        Card C2 = new Card
+        {
+            Id = 2,
+            Name = "Sketchy Sarah",
+            Attack = 2,
+            Defense = 4,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C2);
+        Card C3 = new Card
+        {
+            Id = 3,
+            Name = "Doodle Dave",
+            Attack = 4,
+            Defense = 2,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C3);
+        Card C4 = new Card
+        {
+            Id = 4,
+            Name = "Pencil Pete",
+            Attack = 3,
+            Defense = 5,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C4);
+        Card C5 = new Card
+        {
+            Id = 5,
+            Name = "Marker Mike",
+            Attack = 4,
+            Defense = 4,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C5);
+        Card C6 = new Card
+        {
+            Id = 6,
+            Name = "Eraser Edith",
+            Attack = 2,
+            Defense = 6,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C6);
+        Card C7 = new Card
+        {
+            Id = 7,
+            Name = "Crayon Carla",
+            Attack = 5,
+            Defense = 3,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C7);
+        Card C8 = new Card
+        {
+            Id = 8,
+            Name = "Scribble Sam",
+            Attack = 4,
+            Defense = 5,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C8);
+        Card C9 = new Card
+        {
+            Id = 9,
+            Name = "Inkwell Ivan",
+            Attack = 6,
+            Defense = 2,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C9);
+        Card C10 = new Card
+        {
+            Id = 10,
+            Name = "Paintbrush Penny",
+            Attack = 5,
+            Defense = 4,
+            ImageUrl = imageUrlBase
+        };
+        builder.Entity<Card>().HasData(C10);
+
+
+    }
     public DbSet<Card> Cards { get; set; } = default!;
 
     public DbSet<Player> Players { get; set; } = default!;
