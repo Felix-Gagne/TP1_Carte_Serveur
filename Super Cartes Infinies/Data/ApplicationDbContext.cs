ef0b67fe8fe30ac3bb883350649c7874a4353f62
vlad <2184803@cegepmontpetit.ca>
2023-09-01 13:39:03 -0400

    FCT : Ajout de l'admin dans le data seed

--- Super Cartes Infinies/Data/ApplicationDbContext.cs
+++ Super Cartes Infinies/Data/ApplicationDbContext.cs
@@ -20,6 +20,33 @@ public class ApplicationDbContext : IdentityDbContext
 
         // TODO: Générer le seed
 
+        //Ajout de admin
+        builder.Entity<IdentityRole>().HasData(
+               new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN" }
+           );
+
+        //Creation de l'utilisateur qui est admin
+        PasswordHasher<IdentityUser> hasher = new PasswordHasher<IdentityUser>();
+        IdentityUser admin = new IdentityUser
+        {
+            Id = "11111111-1111-1111-1111-111111111111",
+            UserName = "asd@gmail.",
+            Email = "admin@admin.com",
+            // La comparaison d'identity se fait avec les versions normalisés
+            NormalizedEmail = "ADMIN@ADMIN.COM",
+            NormalizedUserName = "ADMIN@ADMIN.COM",
+            EmailConfirmed = true
+        };
+        // On encrypte le mot de passe
+        admin.PasswordHash = hasher.HashPassword(admin, "Passw0rd!");
+        builder.Entity<IdentityUser>().HasData(admin);
+
+        builder.Entity<IdentityUserRole<string>>().HasData(
+            new IdentityUserRole<string> { UserId = admin.Id, RoleId = "1" }
+        );
+
+
+
         builder.Entity<Match>()
             .HasOne(m => m.PlayerDataA)
             .WithMany()
