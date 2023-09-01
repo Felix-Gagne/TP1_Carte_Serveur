7eff18da8458498cb112918722ff233228b90cdf
Félix Gagné <2070094@cegepmontpetit.ca>
2023-09-01 13:37:56 -0400

    Ajout de CORS
    Ajout des autorisations CORS pour l'utilisation des cookies dans program.cs

--- Super Cartes Infinies/Program.cs
+++ Super Cartes Infinies/Program.cs
@@ -29,6 +29,22 @@ builder.Services.AddScoped<IPlayersService, PlayersService>();
 builder.Services.AddSingleton<WaitingUserService>();
 builder.Services.AddScoped<MatchesService>();
 
+//Ajout de la permission des cookies
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAlmostAll", policy =>
+    {
+        //Spécifier les origines autorisés
+        policy.WithOrigins("http://localhost:4200", "https://localhost:4200");
+
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+
+        //Permettre l'utilisation des cookies
+        policy.AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
