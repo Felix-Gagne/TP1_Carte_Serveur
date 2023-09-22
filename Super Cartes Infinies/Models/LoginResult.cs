e515b9ef986ae80e8e1a96ada13652d600fba4c3
vlad <2184803@cegepmontpetit.ca>
2023-09-22 13:39:09 -0400

    FCT : Ajout du service User.
    Action login et register marche. J'ai aussi modifier le controleur afin d'utiliser le nouveau service.

---
+++ Super Cartes Infinies/Models/LoginResult.cs
@@ -0,0 +1,13 @@
+﻿using Super_Cartes_Infinies.Controllers;
+
+namespace Super_Cartes_Infinies.Models
+{
+    public class LoginResult
+    {
+        
+            public bool Success { get; set; }
+            public MonDTO MonDTO { get; set; }
+            public string Error { get; set; }
+        
+    }
+}
