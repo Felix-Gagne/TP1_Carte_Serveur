741db45a92726755a8f7376f32f0fa014313ddb7
1932943 <1932943@cegepmontpetit.ca>
2023-10-04 17:17:55 -0400

    FCT : Fixed Card Filtrage

--- Super Cartes Infinies/Controllers/DeckController.cs
+++ Super Cartes Infinies/Controllers/DeckController.cs
@@ -13,11 +13,13 @@ namespace Super_Cartes_Infinies.Controllers
     {
         UserManager<IdentityUser> _userManager;
         private readonly ApplicationDbContext _context;
+        public CardService _cardService;
 
-        public DeckController(UserManager<IdentityUser> userManager, ApplicationDbContext context, PlayersService playersService) : base(playersService)
+        public DeckController(UserManager<IdentityUser> userManager, ApplicationDbContext context, PlayersService playersService, CardService cardsService) : base(playersService)
         {
             this._userManager = userManager;
             _context = context;
+            _cardService = cardsService;
         }
 
         [HttpGet]
@@ -26,6 +28,26 @@ namespace Super_Cartes_Infinies.Controllers
             List<Card> result = new List<Card>();
 
             result = playersService.GetPlayerFromUserId(UserId).DeckCard;
+
+            return result;
+        }
+
+        [HttpGet]
+        public List<Card> GetAllCards()
+        {
+            List<Card> result = new List<Card>();
+
+            result = _cardService.GetAllCards().ToList();
+
+            return result;
+        }
+
+        [HttpGet]
+        public List<Card> GetFilteredCards(string FiltreChoisi)
+        {
+            List<Card> result = new List<Card>();
+
+            result = _cardService.GetFilteredCards(FiltreChoisi).ToList();
 
             return result;
         }
---
+++ Super Cartes Infinies/Services/CardService.cs
@@ -0,0 +1,36 @@
+﻿using Super_Cartes_Infinies.Data;
+using Super_Cartes_Infinies.Models;
+
+namespace Super_Cartes_Infinies.Services
+{
+    public class CardService
+    {
+        private ApplicationDbContext _context;
+        public CardService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Card> GetAllCards()
+        {
+            return _context.Cards;
+        }
+
+        public IEnumerable<Card> GetFilteredCards(string filtre)
+        {
+            if (filtre == "Attack")
+            {
+                return _context.Cards.OrderBy(c => c.Attack);
+            }
+            if (filtre == "Defense")
+            {
+                return _context.Cards.OrderBy(c => c.Defense);
+            }
+            if (filtre == "Name")
+            {
+                return _context.Cards.OrderBy(c => c.Name);
+            }
+            return _context.Cards;
+        }
+    }
+}
