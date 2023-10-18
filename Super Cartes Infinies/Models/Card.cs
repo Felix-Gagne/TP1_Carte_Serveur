c7547a8718c8d342e9840b64b8e17c54bdc0a23e
1932943 <1932943@cegepmontpetit.ca>
2023-10-18 16:13:45 -0400

    FCT : Les classes pour les power et cartes
    Les classes sont faite et le liens contient la value du power.

--- Super Cartes Infinies/Models/Card.cs
+++ Super Cartes Infinies/Models/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Super_Cartes_Infinies.Services.Interfaces;
 
 namespace Super_Cartes_Infinies.Models
@@ -12,17 +13,34 @@ namespace Super_Cartes_Infinies.Models
 		public int Defense { get; set; }
 		public string ImageUrl { get; set; } = "";
 
-        public override bool Equals(object other)
+		public override bool Equals(object other)
 		{
 			return other is Card c &&
 				(c.Id, c.Name, c.Attack, c.Defense, c.ImageUrl)
 					.Equals((Id, Name, Attack, Defense, ImageUrl));
+		}
+
+		//Cherche la valeur du power de la carte EX:(Thorn 2)
+        public int GetPowerValue(int powerId)
+        {
+            return cardPowers.Single(c => c.PowerId == powerId && c.CardId == Id).value;
         }
 
 
+		//Verification si la carte contient le power demander
+        public bool HasPower(int powerId)
+		{
+			return cardPowers.Any(c => c.PowerId == powerId && c.CardId == Id);
+		}
+
+
 		[JsonIgnore]
+		[ValidateNever]
 		public virtual List<Player> Players { get; set; }
-    }
+
+		[ValidateNever]
+		public List<CardPower> cardPowers { get; set; }
+	}
 			
 }
 
---
+++ Super Cartes Infinies/Models/Power.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Super_Cartes_Infinies.Models
+{
+    public class Power
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+
+
+        //Voici les ids des différents power.
+        public const int CHARGE_ID = 1;
+        public const int FIRSTSTRIKE_ID = 2;
+        public const int THORNS_ID = 3;
+        public const int HEAL_ID = 4;
+
+        //Voici les ids pour les power que nous on décide
+        //Les noms sont à changer (on choisi les powers et leurs effets)
+        public const int CUSTOMPOWER1_ID = 5;
+        public const int CUSTOMPOWER2_ID = 6;
+
+        [ValidateNever]
+        public List<CardPower> cardPowers { get; set; }
+    }
+}
